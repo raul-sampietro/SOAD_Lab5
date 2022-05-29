2894001711906ef61236d1655752ec3178a1458d
Adria Abad <abadadria@gmail.com>
2022-05-29 20:27:43 +0200

    test

--- test.c
+++ test.c
@@ -12,42 +12,32 @@ void usage() {
 }
 
 int main (int argc, char *argv[]) {
-    printf("hola\n");
     if(argc <= 2) {
         usage();
         exit(1);
     }
-    printf("hola\n");
     int n = atoi(argv[1]);
     int l = atoi(argv[2]);
-    printf("hola\n");
     clock_t begin = clock();
 
     // Creación de ficheros encriptados y no encriptados
     for (int i = 0; i < n; ++i) {
-        printf("ini\n");
         // Crea un fichero sin cifrar y escribe una linea, luego escribe l linias
         FILE *fpr;
         char *x;
-        printf("hola\n");
         sprintf(x, "./out/file%d.txt", i);
         fpr = fopen(x, "w+");
-        printf("hola\n");
         for (int j = 0; j < l; ++j) {
             fprintf(fpr, "ihbfiasbvizvfjhznvlibxvkjxnlvjdbjdhbvjbvjdhbdvjhdjhvbd\n");
         }
-        printf("hola\n");
         fclose(fpr);
-        printf("fin\n");
     }
-    printf("hola\n");
     clock_t end = clock();
 
     FILE *fpr = fopen("results.txt", "a");
     FILE *fp = fopen("raw.txt", "a");
-    fprintf(fpr, "Test for %d files: %ld miliseconds\n", n, end);
-    fprintf(fp, "%d %ld\n", n, end);
+    fprintf(fpr, "Test for %d files: %ld miliseconds\n", n, (end - begin));
+    fprintf(fp, "%d %ld\n", n, (end - begin));
     fclose(fpr);
     fclose(fp);
 }
-
